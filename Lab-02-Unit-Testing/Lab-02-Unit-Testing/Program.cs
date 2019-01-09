868c1248418385c53e8e1c6a768aeaf4344ee6ad
Tulip Pink <pinktulips@hotmail.com>
2019-01-09 13:52:56 -0800

    setting up atm questions

---
+++ Lab-02-Unit-Testing/Lab-02-Unit-Testing/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab_02_Unit_Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What can I help you with today?");
+            Console.WriteLine("1. View Balance");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Add Money");
+            Console.WriteLine("4. Exit");
+
+            int.TryParse(Console.ReadLine(), out int number);
+
+            decimal value;
+            switch(number);
+        }
+
+        public static string ATM(string atmMachine);
+    }
+}
---
+++ Lab-02-Unit-Testing/atmtest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Lab_02_Unit_Testing;
+
+namespace atmtest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
