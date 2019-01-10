ddb149d178090df82c9b68c68136b76abcdd427d
Tulip Pink <pinktulips@hotmail.com>
2019-01-09 18:23:48 -0800

    starting switch statements

--- Lab-02-Unit-Testing/Lab-02-Unit-Testing/Program.cs
+++ Lab-02-Unit-Testing/Lab-02-Unit-Testing/Program.cs
@@ -2,7 +2,7 @@
 
 namespace Lab_02_Unit_Testing
 {
-    public static class Program
+    class Program
     {
         static void Main(string[] args)
         {
@@ -12,12 +12,20 @@ namespace Lab_02_Unit_Testing
             Console.WriteLine("3. Add Money");
             Console.WriteLine("4. Exit");
 
-            int.TryParse(Console.ReadLine(), out int number);
+            Int32.TryParse(Console.ReadLine(), out int number);
 
             decimal value;
-            switch(number);
-        }
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("How much money would you like to deposit?");
+                    Decimal.TryParse(Console.ReadLine(), out value);
+                    Console.WriteLine(AddMoney(value) ? "Your money has been deposited" : "You cannot Deposit");
 
-        public static string ATM(string atmMachine);
+                    break;
+
+                    //public static string ATM(string atmMachine);
+            }
+        }
     }
 }
